996e12895365f7d9ac622be71203b180f8ac7bf4
Tan Nguyen <tannguyen1220@gmail.com>
2022-05-13 09:31:11 +0700

    web skeleton

---
+++ bussiness-card/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/ProfilePicture';
+import ProfilePicture from './components/ProfilePicture';
+
+function App() {
+  return (
+    <div className="business-card">
+      <ProfilePicture />
+    </div>
+  );
+}
+
+export default App;
---
+++ bussiness-card/src/components/ProfilePicture.js
@@ -0,0 +1,7 @@
+export default function ProfilePicture() {
+    return (
+        <div className="profile-picture">
+            <img src="/avatar.jpg" alt="profile-picture" />
+        </div>
+    )
+}

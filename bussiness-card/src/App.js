2f3ce34b01f9233407fca649c82f02b850d9888a
Tan Nguyen <tannguyen1220@gmail.com>
2022-05-13 09:34:27 +0700

    top frame

--- bussiness-card/src/App.js
+++ bussiness-card/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import './components/ProfilePicture';
 import ProfilePicture from './components/ProfilePicture';
+import NameAndJobs from './components/Name&Jobs';
 
 function App() {
   return (
     <div className="business-card">
       <ProfilePicture />
+      <NameAndJobs />
     </div>
   );
 }
---
+++ bussiness-card/src/components/Name&Jobs.js
@@ -0,0 +1,8 @@
+export default function NameAndJobs() {
+    return (
+        <div className="name-and-jobs">
+            <h3>YoggSharon</h3>
+            <p> Front-end developer</p>
+        </div>
+    )
+}

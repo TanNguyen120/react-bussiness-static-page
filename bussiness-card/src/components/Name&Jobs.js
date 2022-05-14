470ecee04b4a8b53836441d847dcc4e8caf51e40
Tan Nguyen <tannguyen1220@gmail.com>
2022-05-14 09:29:53 +0700

    link button style

--- bussiness-card/src/components/Name&Jobs.js
+++ bussiness-card/src/components/Name&Jobs.js
@@ -1,8 +1,13 @@
+const name = 'YoggSharon'
+
+
+
 export default function NameAndJobs() {
     return (
         <div className="name-and-jobs">
-            <h1>YoggSharon</h1>
+            <h1> {name} </h1>
             <p> Front-end developer</p>
+            <h4>{name} Website</h4>
         </div>
     )
 }

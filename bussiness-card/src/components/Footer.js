28c24949c20cc5ae174afdacd37a2c07d2ebc0d8
Tan Nguyen <tannguyen1220@gmail.com>
2022-05-14 10:55:16 +0700

    complete

--- bussiness-card/src/components/Footer.js
+++ bussiness-card/src/components/Footer.js
@@ -1,7 +1,14 @@
+import { AiFillTwitterSquare } from "react-icons/ai";
+import { FaFacebookSquare } from "react-icons/fa";
+import { BsInstagram } from "react-icons/bs";
+import { FaGithubSquare } from "react-icons/fa";
 export default function Footer() {
     return (
         <div className="footer">
-            footer here
+            <AiFillTwitterSquare className="footerIcon" />
+            <FaFacebookSquare className="footerIcon" />
+            <BsInstagram className="footerIcon" />
+            <FaGithubSquare className="footerIcon" />
         </div>
     )
 }

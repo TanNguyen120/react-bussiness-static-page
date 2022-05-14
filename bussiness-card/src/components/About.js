8950e49ec621487aba59333ed0008f6685b3582a
Tan Nguyen <tannguyen1220@gmail.com>
2022-05-14 09:46:01 +0700

    about info and style

--- bussiness-card/src/components/About.js
+++ bussiness-card/src/components/About.js
@@ -2,7 +2,7 @@ export default function About() {
     return (
         <div className="about">
             <h2>About</h2>
-            <p> Your Fate Belong to me </p>
+            <p> One of the greatly malevolent and ancient entities known only as Old Gods, Yogg-Saron held chaotic tyranny over the world of Azeroth in the very distant past, and was responsible for the creation of the Curse of Flesh to "facilitate assimilation" of the Titans' creations. When the Titans waged war against the Old Gods and shattered their citadels, they found that the infestation of the entities had grown so severe that destroying the Old Gods would also lead to the annihilation of Azeroth. Instead, the Pantheon took measures to neutralize the Old Gods' power and bind them within Azeroth for the remainder of its existence.Yogg-Saron was sealed away within the depths of Ulduar in the far northern reaches of the infant world, and had six watchers assigned to it which served as wardens for its near-eternal imprisonment; the Titanic watchers known as Loken, Thorim, Hodir, Tyr, Mimiron and Freya. </p>
         </div>
     )
 }
--- bussiness-card/src/components/Interest.js
+++ bussiness-card/src/components/Interest.js
@@ -1,8 +1,18 @@
+
+
+
 export default function Interest() {
     return (
         <div className="interest">
-            <h2>Interest</h2>
-            <p>Back Stab , laugh in their face, kill both player</p>
+            <h2>Quotes</h2>
+            <p>They are coming for you.
+                Give in to your fear.
+                Kill them all before they kill you.
+                They have turned against you. Now, take your revenge.
+                It WAS your fault.
+                Tell yourself again that these are not truly your friends.
+                You are a pawn of forces unseen.
+                There is no escape. Not in this life, not in the next.</p>
         </div>
     )
 }
